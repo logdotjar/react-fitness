687c0e3c9f2c75713cdf642d770ac4ce2aef83ac
jinareum <jinareum.76@gmail.com>
2023-04-10 03:59:22 +0900

    feat : BrowserRouter import 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Box } from "@mui/material";
+import { Route, Routes } from 'react-router-dom';
+import {Box} from "@mui/material";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
@@ -9,18 +9,16 @@ import Footer from "./components/Footer";
 // excercise/:id => 다이나믹한 렌더링 페이지이동을 위해
 
 const App = () => {
-  return (
-    <Box width="400px" sx={{ width : { xl : '1400px'}}} m="auto">
-      <Navbar />
-        <Router>
-
+    return (
+        <Box width="400px" sx={{width: {xl: '1400px'}}} m="auto">
+            <Navbar/>
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/exercise/:id" element={<ExerciseDetail />}/>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
             </Routes>
-        </Router>
-    </Box>
-  )
+            <Footer/>
+        </Box>
+    )
 }
 
 export default App
