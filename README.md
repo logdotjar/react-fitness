6a60b56c307af46cb63a0c43b27bf859b0285748
jinareum <jinareum.76@gmail.com>
2023-04-11 23:57:26 +0900

    docs : 커밋 규약 추가

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Next.js 기반 API를 사용한 fitness 홈페이지
 2. Styled-components
 
 #### Commit 규약
+- feat : 새로운 기능 추가
+- fix : 버그 수정
+- docs : 문서 관련
+- style : 스타일 변경 (포매팅 수정, 들여쓰기 추가)
+- refactor : 코드 리팩토링
+- build : 빌드 관련 파일 수정
+- ci : CI 설정 파일 수정
+- perf : 성능 개선
+- chore : 그 외 자잘한 수정
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-//index.js : connect application to the DOM
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import {createTheme,ThemeProvider} from "@mui/material";
+
 import GlobalStyle from "./style/GlobalStyles";
 
+const theme = createTheme({
+    typography:{
+        fontFamily:"'NotoSansKr',sans-serif"
+    }
+});
+
+
+//index.js : connect application to the DOM
 // react 18version 이상
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
 root.render(
     <StrictMode>
-        <BrowserRouter>
-            <GlobalStyle/>
-            <App/>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <GlobalStyle/>
+                <App/>
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 )
