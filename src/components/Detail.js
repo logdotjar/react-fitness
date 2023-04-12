ab3f84e31a8157a0b2e41dbb93ae625dfb439371
jinareum <jinareum.76@gmail.com>
2023-04-13 00:30:18 +0900

    feat : 운동 항목별 상세페이지 생성

---
+++ src/components/Detail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Typography, Stack, Button} from "@mui/material";
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({exerciseDetail}) => {
+    // 구조분해할당
+    const {bodyPart, gifUrl, name, target, equipment} = exerciseDetail;
+
+
+    return (
+        <Stack
+            gap="60px"
+            sx={{
+                flexDirection: {lg: 'row'},
+                p: '20px',
+                alignItems: 'center'
+            }}
+        >
+            Detail
+        </Stack>
+    );
+};
+
+export default Detail;
